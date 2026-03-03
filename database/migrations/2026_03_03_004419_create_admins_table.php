9cb26b8281ad604c069a293b957534063c84dc17
Mariam Ayman <maruma2.zezo@gmail.com>
2026-03-03 02:48:51 +0200

    migrations

--- database/migrations/2026_03_03_004419_create_admins_table.php
+++ database/migrations/2026_03_03_004419_create_admins_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->text('permissions')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('admins');
     }
 };
