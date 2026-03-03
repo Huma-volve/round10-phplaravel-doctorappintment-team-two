eb7f1682c9d0d17352ed5503a165c1e9b855d7e3
Mariam Ayman <maruma2.zezo@gmail.com>
2026-03-04 00:26:29 +0200

    notifications

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('test', function () {
-    event response()->json(['message' => 'API is working']);});
+Route::get('test', function (): void {
+    $user = \App\Models\User::first();
+    $NOTIFICATION = new \App\Notifications\AppointmentCreatedNotification(\App\Models\Appointment::first());
+    event(new \App\Events\NotificationBroadcastEvent($user->id, $NOTIFICATION));
+});
