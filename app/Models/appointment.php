225f2ae3c33ac1f51776b861a77fe978b723d8c9
Mohamed Mahmoud <mhmed35432@gmail.com>
2026-03-09 00:02:26 +0200

    update

--- app/Models/appointment.php
+++ app/Models/appointment.php
@@ -6,15 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'doctor_id',
-        'patient_id',
-        'appointment_time',
-        'Status',
-    ];
+    //
 }
