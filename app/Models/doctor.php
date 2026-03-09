ad570d9ead38e8d416817204734499bd826113e6
kareemnabil36 <kn795696@gmail.com>
2026-03-09 22:46:24 +0200

    fix

--- app/Models/doctor.php
+++ app/Models/doctor.php
@@ -3,17 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Doctor extends Model
 {
-    protected $guarded = [];
-    public function user()
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'clinic_id',
+        'specialization_id',
+        'clinic_address',
+        'license_number',
+        'bio',
+        'session_price',
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function clinic()
+    public function chats()
     {
         return $this->hasMany(Chat::class, 'doctor_id');
     }
@@ -22,12 +37,12 @@ class Doctor extends Model
         return $this->belongsTo(Clinic::class);
     }
 
-    public function specialization()
+    public function specialization(): BelongsTo
     {
         return $this->belongsTo(Specialization::class);
     }
 
-    public function reviews()
+    public function reviews(): HasMany
     {
         return $this->hasMany(Review::class);
     }
