4e6dd23a0c73e39c403583bc6bd8a4752730708e
kareemnabil36 <kn795696@gmail.com>
2026-03-09 06:06:46 +0200

    update

--- app/Models/doctor.php
+++ app/Models/doctor.php
@@ -3,56 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Doctor extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'user_id',
-        'clinic_id',
-        'specialization_id',
-        'clinic_address',
-        'license_number',
-        'bio',
-        'session_price',
-    ];
-
-    public function user(): BelongsTo
+    protected $guarded = [];
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function chats()
+    public function clinic()
     {
-        return $this->hasMany(Chat::class, 'doctor_id');
-    public function clinic(): BelongsTo
-    {
-        return $this->belongsTo(clinic::class);
+        return $this->belongsTo(Clinic::class);
     }
 
-    public function specialization(): BelongsTo
+    public function specialization()
     {
-        return $this->belongsTo(specialization::class);
+        return $this->belongsTo(Specialization::class);
     }
 
-    public function reviews(): HasMany
+    public function reviews()
     {
-        return $this->hasMany(review::class);
-    }
-
-    public function favorites(): HasMany
-    {
-        return $this->hasMany(favorite::class);
-    }
-
-    public function availabilitySlots(): HasMany
-    {
-        return $this->hasMany(availability_slot::class);
+        return $this->hasMany(Review::class, 'doctor_id');
     }
 }
